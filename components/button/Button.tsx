a26d7f4db3078b16b9747b7e71810a5e16a4c722
zohreh-youki <zohreh.youki@gmail.com>
2025-01-09 01:27:07 +0330

    initial commit

--- components/button/Button.tsx
+++ components/button/Button.tsx
@@ -3,6 +3,7 @@ import React from "react";
 
 interface ButtonProps {
   label: string;
+  size: "small"| "medium" | "large";
   variant?: "primary" | "secondary";
   onClick?: () => void;
   disabled?: boolean;
@@ -12,17 +13,18 @@ export default function Button({
   label,
   variant = "primary",
   onClick,
-  disabled,
+  disabled,size
 }: ButtonProps) {
   const styles =
     variant === "primary"
       ? "bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600"
       : "bg-gray-300 text-black px-4 py-2 rounded hover:bg-gray-400";
   const disabledStyles = disabled ? "opacity-50 cursor-not-allowed" : "";
+  const smallSize = size === "small" ? "p-[1]" : "p-[20]";
 
   return (
     <button
-      className={`${styles} ${disabledStyles}`}
+      className={`${styles} ${disabledStyles} ${smallSize}`}
       onClick={disabled ? undefined : onClick}>
       {label}
     </button>
